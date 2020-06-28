eecb3a461fd77c83c96c9599350107e7fde112d5
linjian <linjian@paohe.cn>
2020-06-28 16:16:57 +0800

    [v1.0.0 update] first commit

---
+++ precache-manifest.f0318ef36cce1c620865efcee1ec710f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f4a74afa80b44f9ff58c20fa318b451",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2b195b5b5d81ad6c007",
+    "url": "/static/css/2.3cb76441.chunk.css"
+  },
+  {
+    "revision": "2a694aa97283276444bb",
+    "url": "/static/css/main.4ebabe04.chunk.css"
+  },
+  {
+    "revision": "e2b195b5b5d81ad6c007",
+    "url": "/static/js/2.af31717b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.af31717b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a694aa97283276444bb",
+    "url": "/static/js/main.bea4c989.chunk.js"
+  },
+  {
+    "revision": "040a25f075c71649b2ae",
+    "url": "/static/js/runtime-main.89037f95.js"
+  }
+]);
